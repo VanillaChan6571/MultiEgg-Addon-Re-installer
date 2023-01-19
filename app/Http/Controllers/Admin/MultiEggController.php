9c9010c8d8162ba5b2bca939a189fe053b777a33
Buddy Kipperly <101014585+TheBestPenguinYT@users.noreply.github.com>
2023-01-19 08:47:20 -0700

    Added API and fixed some errors.

--- app/Http/Controllers/Admin/MultiEggController.php
+++ app/Http/Controllers/Admin/MultiEggController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
 use DB;
+use DateTime;
 use Illuminate\View\View;
 use Pterodactyl\Models\MultiEgg;
 use Illuminate\View\Factory as ViewFactory;
@@ -38,7 +39,7 @@ class MultiEggController extends Controller
             'plan'=>MultiEggController::getPlan(),
             'client'=>MultiEggController::getIssuee(),
             'business'=>MultiEggController::getBusiness(),
-            'expires'=>MultiEggController::getExpiry(),
+            'expires'=>MultiEggController::prettyDate(),
             'brand'=>MultiEggController::getBrand(),
             'key_decrypted'=>MultiEggController::getKey(),
             'valid'=>MultiEggController::isValid()
@@ -56,7 +57,9 @@ class MultiEggController extends Controller
         }
         return $this->view->make('admin.multiegg.lite', [
             'valid'=>MultiEggController::isValid(),
-            'version'=>$this->version
+            'version'=>$this->version,
+            'rawkeys'=>MultiEggController::getRawKeys(),
+            'information'=>MultiEggController::getData()
         ]);
     }
 
@@ -71,7 +74,9 @@ class MultiEggController extends Controller
         }
         return $this->view->make('admin.multiegg.plus', [
             'valid'=>MultiEggController::isValid(),
-            'version'=>$this->version
+            'version'=>$this->version,
+            'rawkeys'=>MultiEggController::getRawKeys(),
+            'information'=>MultiEggController::getData()
         ]);
     }
 
@@ -83,7 +88,9 @@ class MultiEggController extends Controller
         }
         return $this->view->make('admin.multiegg.pro', [
             'valid'=>MultiEggController::isValid(),
-            'version'=>$this->version
+            'version'=>$this->version,
+            'rawkeys'=>MultiEggController::getRawKeys(),
+            'information'=>MultiEggController::getData()
         ]);
     }
 
@@ -129,19 +136,62 @@ class MultiEggController extends Controller
                 ->pluck('license_key');
 
 
-            $build = "https://license.multiegg.ml/keyInfo.php?v1={$cKey[0]}&v2={$lKey[0]}";
+            $build = "https://api.multiegg.xyz/keyInfo.php?v1={$cKey[0]}&v2={$lKey[0]}";
             $res = Http::get($build)->object();
-
             Cache::put('multiegg_license', $res, now()->addMinutes(60));
         }
         return Cache::get('multiegg_license');
     }
 
+    public function getRawKeys() {
+        $cKey = DB::table('multiegg')
+            ->where('id', '=', '1')
+            ->pluck('confirm_key');
+
+        $lKey = DB::table('multiegg')
+            ->where('id', '=', '1')
+            ->pluck('license_key');
+
+        $cRaw = $cKey[0];
+        $lRaw = $lKey[0];
+
+        $rawKeys = [ "confirm_key" => $cRaw, "license_key" => $lRaw ];
+        return $rawKeys;
+
+    }
+
+    public function getData() {
+        $data = [
+            "domain" => MultiEggController::getDomain(),
+            "watermark" => MultiEggController::getWatermark(),
+            "email" => MultiEggController::getEmail(),
+            "discord" => MultiEggController::getDiscord(),
+        ];
+        return $data;
+    }
+
     public function isValid() {
-        if(MultiEggController::keyValid() && MultiEggController::domainValid()){
+        if(MultiEggController::keyValid() && MultiEggController::domainValid() && MultiEggController::timeValid()){
             return "Active";
         }
         return "Inactive";
+    }
+
+    public function prettyDate() {
+        $date = strtotime(MultiEggController::getExpiry());
+        $prettyDate = date('M d Y', $date);
+        return $prettyDate;
+    }
+
+    public function timeValid() {
+        if(MultiEggController::keyValid()) {
+            $date = new DateTime(MultiEggController::getExpiry());
+            $now = new DateTime();
+            if($date < $now){
+                return false;
+            }
+            return true;
+        }
     }
 
     public function keyValid() {
@@ -163,7 +213,7 @@ class MultiEggController extends Controller
     public function getIssuee() {
         if(MultiEggController::keyValid()){
             $data = MultiEggController::getLicenseDetails();
-            return $data->data->issuee;
+            return $data->client->issuee;
         }
         return "ERROR";
     }
@@ -171,7 +221,7 @@ class MultiEggController extends Controller
     public function getBusiness() {
         if(MultiEggController::keyValid()){
             $data = MultiEggController::getLicenseDetails();
-            return $data->data->issuee_business;
+            return $data->client->issuee_business;
         }
         return "ERROR";
     }
@@ -179,7 +229,7 @@ class MultiEggController extends Controller
     public function getExpiry() {
         if(MultiEggController::keyValid()){
             $data = MultiEggController::getLicenseDetails();
-            return $data->data->expires;
+            return $data->keyinfo->expires;
         }
         return "ERROR";
     }
@@ -187,7 +237,7 @@ class MultiEggController extends Controller
     public function getPlan() {
         if(MultiEggController::keyValid()){
             $data = MultiEggController::getLicenseDetails();
-            return $data->data->plan;
+            return $data->keyinfo->plan;
         }
         return "ERROR";
     }
@@ -195,7 +245,7 @@ class MultiEggController extends Controller
     public function getBrand() {
         if(MultiEggController::keyValid()){
             $data = MultiEggController::getLicenseDetails();
-            return $data->data->custom_brand;
+            return $data->custombranding->whitelabel;
         }
         return "ERROR";
     }
@@ -203,7 +253,31 @@ class MultiEggController extends Controller
     public function getDomain() {
         if(MultiEggController::keyValid()){
             $data = MultiEggController::getLicenseDetails();
-            return $data->data->valid_domain;
+            return $data->custombranding->valid_domain;
+        }
+        return "ERROR";
+    }
+
+    public function getEmail() {
+        if(MultiEggController::keyValid()){
+            $data = MultiEggController::getLicenseDetails();
+            return $data->custombranding->email;
+        }
+        return "ERROR";
+    }
+
+    public function getDiscord() {
+        if(MultiEggController::keyValid()){
+            $data = MultiEggController::getLicenseDetails();
+            return $data->custombranding->discord;
+        }
+        return "ERROR";
+    }
+
+    public function getWatermark() {
+        if(MultiEggController::keyValid()){
+            $data = MultiEggController::getLicenseDetails();
+            return $data->custombranding->whitelabel;
         }
         return "ERROR";
     }
@@ -234,6 +308,15 @@ class MultiEggController extends Controller
     private function denied() {
         $this->alert->warning("You do not have access to this plan.")->flash();
         return redirect()->back();
+    }
+
+    public function clearCache() {
+        if(Cache::has('multiegg_license')){
+                Cache::forget('multiegg_license');
+        }
+        MultiEggController::getLicenseDetails();
+        $this->alert->success('License Key Info Successfully Updated')->flash();
+        return redirect()->route('admin.multiegg.index');
     }
 
 }
