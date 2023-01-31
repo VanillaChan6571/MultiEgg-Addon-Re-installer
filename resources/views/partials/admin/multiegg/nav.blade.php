758b25d765869495cf9938fe98dc081b2411d4da
ExerpimentalX <chrlew082@gmail.com>
2023-01-31 19:26:40 +0000

    Update nav.blade.php

--- resources/views/partials/admin/multiegg/nav.blade.php
+++ resources/views/partials/admin/multiegg/nav.blade.php
@@ -8,9 +8,6 @@
             <div class="nav-tabs-custom nav-tabs-floating">
                 <ul class="nav nav-tabs">
                     <li @if($activeTab === 'overview')class="active"@endif><a href="{{ route('admin.multiegg.index') }}">Overview</a></li>
-                    <li @if($activeTab === 'lite')class="active"@endif><a href="{{ route('admin.multiegg.lite') }}">Lite</a></li>
-                    <li @if($activeTab === 'plus')class="active"@endif><a href="{{ route('admin.multiegg.plus') }}">Plus</a></li>
-                    <li @if($activeTab === 'pro')class="active"@endif><a href="{{ route('admin.multiegg.pro') }}">Pro</a></li>
                     <li @if($activeTab === 'support')class="active"@endif><a href="{{ route('admin.multiegg.support') }}">Support</a></li>
                 </ul>
             </div>
