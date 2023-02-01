0bb69928bfea096d482b30032defe642cf191d90
Buddy Kipperly <101014585+TheBestPenguinYT@users.noreply.github.com>
2023-01-31 19:44:30 -0700

    Update support.blade.php

--- resources/views/admin/multiegg/support.blade.php
+++ resources/views/admin/multiegg/support.blade.php
@@ -1,3 +1,4 @@
+
     // please no remove, the other tabs are literally identical to yours, and if you tryna access a higher plan, the api literally smack you across t>
     // sorry there, but welp, I guess that's one way to prevent pervs from tryna see the code nonsense
     //
@@ -28,6 +29,40 @@
 @section('content')
 @yield('multiegg::nav')
 
-<strong>Unfortunately, this section is not complete. Please use Discord Support Tickets for now.</strong>
+
+<div class="row" style="padding-left: 14px; padding-right: 14px">
+    <div class="sol-sm-8">
+        <form action="some link here" method="POST">
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Support Form</h3>
+            </div>
+
+            <div class="box-body">
+                <div class="form-group">
+                    {{ Form::label('email', 'Email') }}
+                    {{ Form::email('email', '', array('class' => 'form-control')) }}
+                </div>
+                <div class="form-group">
+                    {{ Form::label('subject', 'Subject') }}
+                    {{ Form::text('subject', 'I need help with...', array('class' => 'form-control')) }}
+                </div>
+                <div class="form-group">
+                    {{ Form::label('body', 'Description') }}
+                    <textarea name="body" class="form-control" rows="5"></textarea>
+                </div>
+                <div>
+                    <textarea class="form-control" rows="2" readonly>By submitting this form, you consent for your panel Domain, IP, Addon Version, and other addons / license details to be shared with our support team. (We will get back to you within 3 business days)</textarea>
+                </div>
+                <div class="form-group">
+                    {{ Form::submit('Send', array('class' => 'btn btn-smm btn-primary pull-right')) }}
+                </div>
+            </div>
+
+        </div>
+        </form>
+    </div>
+</div>
+
 
 @endsection
