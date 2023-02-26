45b7b4865789348e03bb77868020d5857756d2fc
ExerpimentalX <chrlew082@gmail.com>
2023-02-26 14:18:30 -0700

    Update index.blade.php

--- resources/views/admin/multiegg/index.blade.php
+++ resources/views/admin/multiegg/index.blade.php
@@ -1,28 +1,5 @@
 use app\Http\Controllers\MultiEggController;
 
-@section('multiegg::notice')
-    @if($valid == 'Inactive')
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="alert alert-danger">
-                    Your license key is invalid. Please make sure you have an active plan/key. If you belive this is wrong, contact support.
-                </div>
-            </div>
-        </div>
-    @endif
-    @if($global_settings->latest_version != $global_settings->current_version )
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="alert alert-warning">
-                    Your addon is outdated! Please update it as soon as you can! You are on version {{ $global_settings->current_version }}, and the latest is {{ $global_settings->latest_version }}.
-                </div>
-            </div>
-        </div>
-    @endif
-@endsection
-root@extronode:~# cat /var/www/pterodactyl/resources/views/admin/multiegg/index.blade.php
-use app\Http\Controllers\MultiEggController;
-
 <style>
         .extraPadd { padding-left: 100px }
         .no-resize { resize: none; }
